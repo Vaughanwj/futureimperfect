55066e78db42c498dd0ffbd0be2eec4de3814f95
Vaughan Wynne-Jones <vaughanwj@gmail.com>
2026-03-25 23:30:47 -0500

    favicon added

--- add.bat
+++
@@ -1,5 +0,0 @@
-#The . in git add . catches everything that changed in the folder. Then give GitHub Pages a minute or two to rebuild and refresh the site.
-
-git add .
-git commit -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
-git push
